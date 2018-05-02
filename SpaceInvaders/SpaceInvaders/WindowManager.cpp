80926eeaaf2bf8c8fd4e20e00f963cc039506814
AlexAhnon <alexahnon@gmail.com>
2018-05-02 15:22:09 +0200

    Endret på WindowManager og main

--- SpaceInvaders/SpaceInvaders/WindowManager.cpp
+++ SpaceInvaders/SpaceInvaders/WindowManager.cpp
@@ -14,12 +14,64 @@ void WindowManager::Release() {
 	instance = NULL;
 }
 
+int WindowManager::SetupWindow()
+{
+	// Initialize SDL, return error if failed
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return EXIT_FAILURE;
+	}
+
+	// Error checking for window
+	if (InitializeWindow() != 0) {
+		std::cerr << "Failed to create window. SDL_Error: " << SDL_GetError() << std::endl;
+		DeleteWindow();
+		return EXIT_FAILURE;
+	}
+
+	// Error checking for renderer
+	if (InitializeRenderer() != 0) {
+		std::cerr << "Failed to create renderer. SDL_Error: " << SDL_GetError() << std::endl;
+		DeleteWindow();
+		return EXIT_FAILURE;
+	}
+
+	// Error checking for surface
+	if (InitializeSurface() != 0) {
+		std::cerr << "Failed to create surface and textures. SDL_Error: " << SDL_GetError() << std::endl;
+		DeleteWindow();
+		return EXIT_FAILURE;
+	}
+
+	// Error checking for texture
+	if (InitializeTexture() != 0) {
+		std::cerr << "Failed to create surface and textures. SDL_Error: " << SDL_GetError() << std::endl;
+		DeleteWindow();
+		return EXIT_FAILURE;
+	}
+
+	// Render it to the screen
+	SDL_RenderCopy(renderer, texture, NULL, NULL);
+	SDL_RenderPresent(renderer);
+}
+
+void WindowManager::DeleteWindow()
+{
+	SDL_DestroyTexture(texture);
+	SDL_FreeSurface(surface);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
 WindowManager::WindowManager()
 {
 	window = nullptr;
 	gameTitle = "Space Invaders";
 	screenHeight = 550;
 	screenWidth = 500;
+	bgImage = "../Artwork/invaders.bmp";
 }
 
 
@@ -46,7 +98,59 @@ int WindowManager::InitializeWindow()
 	return 0;
 }
 
+int WindowManager::InitializeRenderer()
+{
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	if (renderer == nullptr) {
+		return 1;
+	}
+
+	// Turn renderer background to black
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	return 0;
+}
+
+int WindowManager::InitializeSurface()
+{
+	surface = SDL_LoadBMP(bgImage);
+
+	if (surface == nullptr) {
+		return 1;
+	}
+
+	return 0;
+}
+
+int WindowManager::InitializeTexture()
+{
+	texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+	if (texture == nullptr) {
+		return 1;
+	}
+
+	return 0;
+}
+
 SDL_Window * WindowManager::getWindow()
 {
 	return window;
+}
+
+SDL_Renderer * WindowManager::getRenderer()
+{
+	return renderer;
+}
+
+SDL_Surface * WindowManager::getSurface()
+{
+	return surface;
+}
+
+SDL_Texture * WindowManager::getTexture()
+{
+	return texture;
 }
--- SpaceInvaders/SpaceInvaders/WindowManager.h
+++ SpaceInvaders/SpaceInvaders/WindowManager.h
@@ -8,11 +8,27 @@ public:
 	static WindowManager* getInstance();
 	static void Release();
 
+	int SetupWindow();
+	void DeleteWindow();
+
 	int InitializeWindow();
+	int InitializeRenderer();
+	int InitializeSurface();
+	int InitializeTexture();
+
 	SDL_Window* getWindow();
+	SDL_Renderer* getRenderer();
+	SDL_Surface* getSurface();
+	SDL_Texture* getTexture();
 
 private:
-	SDL_Window * window;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	SDL_Surface* surface;
+	SDL_Texture* texture;
+
+	const char* bgImage;
+
 	const char* gameTitle;
 	int screenWidth;
 	int screenHeight;
